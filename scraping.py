99c19ffcef37540e3e734fcd2ffb19ffbd0cb6b8
danilevas <dlevacov@gmail.com>
2025-04-15 23:52:11 -0300

    andando

--- scraping.py
+++ scraping.py
@@ -4,32 +4,48 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import os
 
-# Função 1: configurar e iniciar o driver do Selenium
+# Configurar e iniciar o driver do Selenium
 def iniciar_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
-# Função 2: obter o HTML da página
+# Rola a página até o final para carregar todo o PDF
+def rolar_ate_o_fim(driver):
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(5)  # espera carregar
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+# Obter o HTML da página
 def obter_html(url):
     driver = iniciar_driver()
     driver.get(url)
-    time.sleep(5)  # aguarda carregamento
+    print("\nRolando página para carregar todo conteúdo...")
+    rolar_ate_o_fim(driver)
+    print("Capturando HTML final")
     html = driver.page_source
     driver.quit()
     return html
 
-# Função 3: salvar o HTML em um arquivo local
-def salvar_html(html, nome_arquivo="diario_oficial.html"):
+# Salvar o HTML em um arquivo local
+def salvar_html(html, nome_arquivo=f"diario_oficial.html"):
     with open(nome_arquivo, "w", encoding="utf-8") as f:
         f.write(html)
 
-# Função 4: extrair dados do HTML
-def extrair_dados(html):
+# Extrair dados do HTML
+def extrair_dados(data, html):
     soup = BeautifulSoup(html, "html.parser")
     texto = soup.get_text(separator=" ", strip=True)
+    with open(f"txts/texto_html_{data}.txt", "w", encoding="utf-8") as f:
+        f.write(texto)
     
     # Encontrar todas as instâncias de "NOMEAR" (ignorando tags)
     padrao_nomeacao = r'NOMEAR(?:-SE)?(?:\s*<[^>]+>\s*)*(?P<nome>[A-ZÁ-ÚÂ-ÛÃ-ÕÉ-ÍÓ-ÚÇ\s\-]+)'
@@ -56,6 +72,10 @@ def extrair_dados(html):
             de = re.search("de", cargo_limpo)
             cargo_limpo = cargo_limpo[de.end():-1].lstrip()
             cargo_limpo = cargo_limpo.replace("  ", " ")
+
+            # Tirar as instâncias de "- " que é simplesmente uma quebra de linha no meio da palavra,
+            # sem tirar as instâncias de " - ", um hífen mesmo
+            cargo_limpo = re.sub(r'(?<! )- ', '', cargo_limpo)
         else:
             cargo_limpo = "Cargo não encontrado"
         
@@ -131,31 +151,72 @@ def extrair_dados(html):
     
     return nomeacoes, cargos, simbolos, lotacoes
 
-# Função 5: salvar os dados em CSV
-def salvar_em_csv(nomeacoes, cargos, simbolos, lotacoes, nome_arquivo="dados.csv"):
-    df = pd.DataFrame({"Nome": nomeacoes, "Cargo": cargos, "Símbolo": simbolos, "Lotação": lotacoes})
-    df.to_csv(nome_arquivo, index=False, encoding="utf-8-sig")
+# Salvar os dados em CSV
+def salvar_em_csv(data, nomeacoes, cargos, simbolos, lotacoes, nome_arquivo="csvs/dados.csv"):
+    lista_data = [data] * len(nomeacoes)
+    df_novo = pd.DataFrame({"Data DO": lista_data, "Nome": nomeacoes, "Cargo": cargos, "Símbolo": simbolos, "Lotação": lotacoes})
+
+    try:
+        # Tenta carregar um CSV existente
+        df_existente = pd.read_csv(nome_arquivo, encoding="utf-8-sig")
+        df_final = pd.concat([df_existente, df_novo], ignore_index=True)
+    except FileNotFoundError:
+        # Se o arquivo ainda não existir, usa apenas o novo
+        df_final = df_novo
+
+    df_final.to_csv(nome_arquivo, index=False, encoding="utf-8-sig")
+    return df_final
+
+# Função principal (executa tudo)
+def executar_fluxo_completo(urls):
+    # Deleta o arquivo com dados, se existir, para pegarmos dados novos
+    if os.path.isfile("csvs/dados.csv"):
+        os.remove("csvs/dados.csv")
+
+    # Iterando pelos dias e os URLs com seus DOs
+    for data, url in urls.items():
+        print(f"\n > Obtendo html do DO do dia {data}")
+        html = obter_html(url)
+        print(f"Salvando html do DO do dia {data}")
+        salvar_html(html, nome_arquivo=f"htmls/diario_oficial_{data}.html")
+        print(f"Extraindo dados do DO do dia {data}")
+        nomes, cargos, simbolos, lotacoes = extrair_dados(data, html)
+        print(f"Salvando o CSV do DO do dia {data}")
+        df = salvar_em_csv(data, nomes, cargos, simbolos, lotacoes)
+    
     return df
 
 # Função principal (executa tudo)
-def executar_fluxo_completo(url):
-    html = obter_html(url)
-    salvar_html(html)
-    nomes, cargos, simbolos, lotacoes = extrair_dados(html)
-    df = salvar_em_csv(nomes, cargos, simbolos, lotacoes)
+def executar_fluxo_um_dia(urls, data):
+    # Deleta o arquivo com dados, se existir, para pegarmos dados novos
+    if os.path.isfile(f"csvs/dados_teste_{data}.csv"):
+        os.remove(f"csvs/dados_teste_{data}.csv")
+
+    print(f"\n > Obtendo html do DO do dia {data}")
+    html = obter_html(urls[data])
+    print(f"Salvando html do DO do dia {data}")
+    salvar_html(html, nome_arquivo=f"htmls/diario_oficial_{data}_teste.html")
+    print(f"Extraindo dados do DO do dia {data}")
+    nomes, cargos, simbolos, lotacoes = extrair_dados(data, html)
+    print(f"Salvando o CSV do DO do dia {data}")
+    df = salvar_em_csv(data, nomes, cargos, simbolos, lotacoes, nome_arquivo=f"csvs/dados_teste_{data}.csv")
     return df
 
-def procurar_no_html_salvo():
-    with open("diario_oficial.html", encoding="utf-8") as f:
+def procurar_no_html_salvo(data):
+    with open(f"htmls/diario_oficial_{data}.html", encoding="utf-8") as f:
         html_salvo = f.read()
-    nomes, cargos, simbolos, lotacoes = extrair_dados(html_salvo)
-    df = salvar_em_csv(nomes, cargos, simbolos, lotacoes)
+    print(f"Extraindo dados do DO do dia {data}")
+    nomes, cargos, simbolos, lotacoes = extrair_dados(data, html_salvo)
+    print(f"Salvando o CSV do DO do dia {data}")
+    df = salvar_em_csv(data, nomes, cargos, simbolos, lotacoes, nome_arquivo=f"csvs/dados_teste_{data}.csv")
     return df
 
-urls = ["https://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?session=VWtSQ1JsRlZUa1JPUkUxMFRVUkdSVTFUTURCT2FrNUhURlJvUlUwd1JYUlJhMDE1VG1wQmVWRlVRa1ZOZWxVeQ==",
-        "https://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?session=VG1wb1IxRnFWa1JPVkZGMFRVUlplVTlUTURCU1JWRjZURlZKZDA1clRYUk9hbU0wVFVWUk5FMUVUVFJSVkdzeg==",
-        "https://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?session=VW1wWk1rOUZTa2ROVkdkMFVWUktSbEZUTURCUmVrRTFURlZKZVUwd1NYUlJhbGsxVWxSVmVFMHdUVEJTYW1oRA=="]
+urls = {
+    "2025-04-07" : "https://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?session=VWtSQ1JsRlZUa1JPUkUxMFRVUkdSVTFUTURCT2FrNUhURlJvUlUwd1JYUlJhMDE1VG1wQmVWRlVRa1ZOZWxVeQ==",
+    "2025-04-08" : "https://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?session=VG1wb1IxRnFWa1JPVkZGMFRVUlplVTlUTURCU1JWRjZURlZKZDA1clRYUk9hbU0wVFVWUk5FMUVUVFJSVkdzeg==",
+    "2025-04-09" : "https://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?session=VW1wWk1rOUZTa2ROVkdkMFVWUktSbEZUTURCUmVrRTFURlZKZVUwd1NYUlJhbGsxVWxSVmVFMHdUVEJTYW1oRA==",
+    "2025-04-10" : "https://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?session=VG1wVmVWRnFaelZTUkVGMFVWUkpNRkY1TURCUk1FcENURlZKZDA1VVRYUlBWVTE1VDBSa1IwMUZVVFZOTUZWMw==",
+    "2025-04-11" : "https://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?session=VFZSQk5WRnJWVEpSVkd0MFVtcG9SVTFETURCTmVrcEdURlZKTlUxNldYUlJlbGw2VDFST1IwMUVUVEZQVkZWNA=="
+}
 
-
-# df_nomes = procurar_no_html_salvo()
-df_nomes = executar_fluxo_completo(urls[0])
+df = executar_fluxo_um_dia(urls, "2025-04-08")
